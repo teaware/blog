4ddeb3326b70ffdfe6ed66ae08fc18d5e5fd923f
George Brown <teawares@gmail.com>
2020-08-10 11:56:38 +0800

    remove bio

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,78 +4,79 @@ import { kebabCase } from "lodash"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Lettering from "../components/lettering"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Comment from "../components/comment"
 import { rhythm } from "../utils/typography"
 
-class Post extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const commentBox = React.createRef()
+const Post = ({ data, location }) => {
+  const post = data.markdownRemark
+  const siteTitle = data.site.siteMetadata.title
+  const commentBox = React.createRef()
 
-    useEffect(() => {
-      const commentEl = document.createElement("script")
-      commentEl.async = true
-      commentEl.src = "https://utteranc.es/client.js"
-      commentEl.setAttribute("repo", "teaware/comments")
-      commentEl.setAttribute("issue-term", "pathname")
-      commentEl.setAttribute("id", "utterances")
-      commentEl.setAttribute("theme", "github-light")
-      commentEl.setAttribute("crossorigin", "anonymous")
-      if (commentBox && commentBox.current) {
-        commentBox.current.appendChild(commentEl)
-      } else {
-        console.log(`Error adding utterances comments on: ${commentBox}`)
-      }
-    }, [])
+  useEffect(() => {
+    const scriptEl = document.createElement("script")
+    scriptEl.async = true
+    scriptEl.src = "https://utteranc.es/client.js"
+    scriptEl.setAttribute("repo", "teaware/comments")
+    scriptEl.setAttribute("issue-term", "pathname")
+    scriptEl.setAttribute("id", "utterances")
+    scriptEl.setAttribute("theme", "github-light")
+    scriptEl.setAttribute("crossorigin", "anonymous")
+    if (commentBox && commentBox.current) {
+      commentBox.current.appendChild(scriptEl)
+    } else {
+      console.log(`Error adding utterances comments on: ${commentBox}`)
+    }
+  }, [])
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <article className="post">
+        <header>
+          <h2>
+            <Lettering title={post.frontmatter.title} />
+          </h2>
+          <p className="date">{post.frontmatter.date}</p>
+          {post.frontmatter.tags ? (
+            <ul className="tags">
+              {post.frontmatter.tags.map(tag => (
+                <li key={tag + `tag`}>
+                  <AniLink
+                    cover
+                    direction="down"
+                    bg="#ccb833"
+                    to={`/tags/${kebabCase(tag)}`}
+                  >
+                    #{tag}
+                  </AniLink>
+                </li>
+              ))}
+            </ul>
+          ) : null}
+        </header>
+        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
         />
-        <article className="post">
-          <header>
-            <h2>
-              <Lettering title={post.frontmatter.title} />
-            </h2>
-            <p className="date">{post.frontmatter.date}</p>
-            {post.frontmatter.tags ? (
-              <ul className="tags">
-                {post.frontmatter.tags.map(tag => (
-                  <li key={tag + `tag`}>
-                    <AniLink
-                      cover
-                      direction="down"
-                      bg="#ccb833"
-                      to={`/tags/${kebabCase(tag)}`}
-                    >
-                      #{tag}
-                    </AniLink>
-                  </li>
-                ))}
-              </ul>
-            ) : null}
-          </header>
-          <section dangerouslySetInnerHTML={{ __html: post.html }} />
-          <hr
-            style={{
-              marginBottom: rhythm(1),
-            }}
-          />
-          <Bio />
-          <section id="comments">
-            <h2>Comments</h2>
-            <Comment commentBox={commentBox} />
-          </section>
-        </article>
-      </Layout>
-    )
-  }
+        <section
+          id="comments"
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        >
+          <h3>Comments</h3>
+          <Comment commentBox={commentBox} />
+        </section>
+      </article>
+    </Layout>
+  )
 }
 
 export default Post
