3a2755311262579b1e0f74f242544920e5ce33ad
George Brown <teawares@gmail.com>
2020-11-23 12:11:47 +0800

    display: "standalone",

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,7 +119,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#000000`,
         theme_color: `#000000`,
-        display: `minimal-ui`,
+        display: "standalone",
         icon: `src/assets/icon-white.png`,
       },
     },
